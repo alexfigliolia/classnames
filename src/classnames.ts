630462cde6a3ae1088203a5d80896797d77f0adc
Alex Figliolia <alexfigliolia@gmail.com>
2024-08-02 13:39:13 -0400

    Refactor

--- src/classnames.ts
+++ src/classnames.ts
@@ -3,28 +3,16 @@ import type { IClassNameList } from "./types";
 export const classnames = (...args: IClassNameList) => {
   const classes: string[] = [];
   for (const arg of args) {
-    switch (typeof arg) {
-      case "string":
-        classes.push(arg);
-        break;
-      case "object": {
-        if (!arg) {
-          break;
+    if (typeof arg === "string" && arg.length) {
+      classes.push(arg);
+    } else if (Array.isArray(arg)) {
+      classes.push(...classnames(arg));
+    } else if (typeof arg === "object" && !!arg) {
+      for (const key in arg) {
+        if (arg[key]) {
+          classes.push(key);
         }
-        if (Array.isArray(arg)) {
-          classes.push(...classnames(arg));
-          break;
-        }
-        for (const key in arg) {
-          if (arg[key]) {
-            classes.push(key);
-          }
-        }
-        break;
       }
-      case "undefined":
-      default:
-        break;
     }
   }
   return classes.join(" ");
